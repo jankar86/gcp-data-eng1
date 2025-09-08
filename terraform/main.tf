50f767c228c984d5bd145a67fb5070f52a48475d
jankar <psbailey86@gmail.com>
2025-09-08 13:57:33 -0500

    add readme and tf code

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Enable core APIs
+resource "google_project_service" "services" {
+  for_each = toset([
+    "bigquery.googleapis.com",
+    "bigquerydatatransfer.googleapis.com",
+    "storage.googleapis.com",
+    "pubsub.googleapis.com",
+    "dataflow.googleapis.com",
+    "composer.googleapis.com"
+  ])
+  project = var.project_id
+  service = each.key
+}
+
+# Storage bucket for raw data
+resource "google_storage_bucket" "raw_data" {
+  name                        = "${var.project_id}-raw-data"
+  location                    = var.region
+  uniform_bucket_level_access = true
+}
+
+# BigQuery dataset
+resource "google_bigquery_dataset" "lab_dataset" {
+  dataset_id = "lab_dataset"
+  project    = var.project_id
+  location   = var.region
+}
+
+# Pub/Sub topic
+resource "google_pubsub_topic" "events" {
+  name    = "lab-events"
+  project = var.project_id
+}
